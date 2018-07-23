05b8e425985da6edfcc83c5370f0c06f2a67a5ea
Zeshawn Khan <zeshawnk@gmail.com>
2018-07-23 04:45:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var  total;
 
 function add(a,b){
@@ -44,3 +45,9 @@ function preserveDecimal(n){
   total = parseFloat(n);
   return total;
 }
+=======
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
+>>>>>>> 1936a5284e5bf8119e5e025af48c65dd965304ec
